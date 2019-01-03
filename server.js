407e66ec3b24e52fc42f48faa5ef646229de141f
some name <some-address@example.com>
2019-01-03 17:07:38 +0900

    update

--- server.js
+++ server.js
@@ -438,7 +438,7 @@ client.on('ready', async message => {
     client.user.setPresence({
         status: 'online',
         game: {
-            name: '$help　new:R18',
+            name: '$help　new:文字 [文章]',
             url: 'https://lil-sis-counter.glitch.me/'
         }
     });
@@ -702,6 +702,78 @@ function runCommand(client, message) {
                 })
                 return;
             }
+            if (message.content.includes('マルコフ')) {
+                var moji = require('kuromoji');
+                var builder = moji.builder({
+                  dicPath: 'node_modules/kuromoji/dict'
+                });
+                class Markov {
+                  constructor(n) {
+                    this.data = {};
+                  }
+                // データ登録
+                  add(words) {
+                    for(var i = 0; i <= words.length; i++) {
+                      var now = words[i];
+                      if(now === undefined) { now = null };
+                      var prev = words[i - 1];
+                      if(prev === undefined) { prev = null };
+                      
+                      if(this.data[prev] === undefined) {
+                        this.data[prev] = [];
+                      }
+                      this.data[prev].push(now);
+                    }
+                  }
+                // 指定された文字に続く文字をランダムに返す
+                  sample(word) {
+                    var words = this.data[word];
+                    if(words === undefined) { words = []; }
+              
+                    return words[Math.floor(Math.random() * words.length)];
+                  }
+                // マルコフ連鎖でつなげた文を返す
+                  make() {
+                    var sentence = [];
+                    var word = this.sample(null);
+                    while(word) {
+                      sentence.push(word);
+                      word = this.sample(word);
+                    }
+                    return sentence.join('');
+                  }
+                }
+              
+                var markov = new Markov();
+              
+                builder.build(function(err, tokenizer) {
+                  if(err) { throw err; }
+              
+                  // アシタカ先輩の台詞を読み込む
+                  (process.tundere.txt, 'utf8', function(err, data) {
+                    if(err) { throw err; }
+              
+                    var lines = data.split("\n"); // 一行ごとに分割
+                    lines.forEach(function(line) {
+                      var tokens = tokenizer.tokenize(line);
+              
+                    // トークンを文中表記にすべて変換
+                      var words = tokens.map(function(token) {
+                        return token.surface_form;
+                      });
+              
+                    // データを登録
+                      markov.add(words);
+                    });  
+              
+                    // 10回くらい生成してみる
+                    for(var n = 0; n < 10; n++) {
+                      console.log(markov.make());
+                    }
+                  });
+                });
+                return;
+              }
             if (message.content.includes('心情')) {
                 var indico = require('indico.io');
                 indico.apiKey = process.env.indico_api_key
